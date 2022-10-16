b204dc9a7c1cbae989c79303884668b3d6aeb34e
B.K.Jayasundera <57324571+bkjaya2020@users.noreply.github.com>
2022-10-16 07:48:31 +0530

    Create firstrun.sh

---
+++ firstrun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mysql -uroot --password=""< /usr/share/zoneminder/db/zm_create.sql 2>/dev/null
+mysql -e "CREATE USER 'zmuser'@localhost IDENTIFIED BY 'zmpass';"
+mysql -e "GRANT ALL PRIVILEGES ON zm.* TO 'zmuser'@'localhost' WITH GRANT OPTION;"
+mysql -e "FLUSH PRIVILEGES ;"
+mysqladmin -uroot --password="" reload 2>/dev/null
+exit
